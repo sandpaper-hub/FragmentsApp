10eaa01b5884ab8f55f334253fd5ef5142daf6d5
Constantine Kim <electro.passion92@gmail.com>
2020-07-26 21:05:50 +0900

    Changed ContentFragment.java Added method View onCreateView();

--- app/src/main/java/com/example/constantinekim/fragmentsapp/ContentFragment.java
+++ app/src/main/java/com/example/constantinekim/fragmentsapp/ContentFragment.java
@@ -1,4 +1,35 @@
 package com.example.constantinekim.fragmentsapp;
 
-public class ContentFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.Date;
+
+public class ContentFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+        Button updateButton = view.findViewById(R.id.updateButton);
+        final TextView updateBox = view.findViewById(R.id.textBox);
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentDate = new Date().toString();
+                updateBox.setText(currentDate);
+            }
+        });
+
+        return view;
+    }
 }
