a06e2dc314499df7011543b851a088fe715716a2
Constantine Kim <electro.passion92@gmail.com>
2020-07-26 20:38:57 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/constantinekim/fragmentsapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.constantinekim.fragmentsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
